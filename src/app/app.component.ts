3485f5191f1e05760a8593cb707cbc8b6a459361
jksolucoesti <jksolucoesti@gmail.com>
2021-12-17 14:00:51 -0300

    Ajustes de html/css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Todo } from 'src/model/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,77 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+
+    public  mode = 'list';
+    public todos : Todo[] =[];
+    public title : string = "Minhas Tarefas";
+
+    public form!: FormGroup;
+
+    constructor(private fb: FormBuilder) {    
+      this.form = this.fb.group({
+        title:['',Validators.compose([
+          Validators.minLength(3),
+          Validators.maxLength(60),
+          Validators.required
+        ])]
+      });
+      this.load();
+    }
+
+    AlterarTexto(){
+      this.title = "Texto alterado";
+    }
+
+    remove(todo : Todo){
+      const index = this.todos.indexOf(todo);
+      if(index !== -1){
+        this.todos.splice(index,1);
+      }
+    }
+    
+
+    markAsDone(todo: Todo){
+      todo.done = true;
+      this.save();
+    }
+
+    markAsUndone(todo : Todo){
+      todo.done = false;
+      this.save();
+    }
+
+    add(){
+      //this.form.value [title]= valor
+      const title = this.form.controls['title'].value;
+      const id = this.todos.length + 1;
+      this.todos.push(new Todo(id,title,false));
+      this.save();
+      this.clear();
+  
+    }
+
+    clear(){
+      this.form.reset();
+    }
+
+    save(){
+      const data = JSON.stringify(this.todos);
+      localStorage.setItem('data',data);
+      this.mode = "list";
+    }
+
+    load(){
+      const data = localStorage.getItem('data');
+      if(data){
+      this.todos = JSON.parse(data);
+      }else{
+      this.todos = [];
+      }
+    }
+
+    changeMode(mode :string){
+      this.mode = mode;
+    }
+
 }
